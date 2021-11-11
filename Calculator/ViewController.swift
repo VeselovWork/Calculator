dd48e2d07e688f1ad7a6de8a0d3e7a8ca880227e
AW <veselovwork@gmail.com>
2021-11-11 11:22:04 +0300

    "+" and "-" add

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController, UITableViewDelegate{
     
     func createButton(_ number: Int, x: Int, y: Int) -> UIButton {
         let unoButton = UIButton (type: .system) as UIButton
-        unoButton.backgroundColor = .red
+        unoButton.backgroundColor = .gray
+        unoButton.setTitleColor(.white, for: .normal)
         unoButton.setTitle("\(number)", for: .normal)
         unoButton.frame = CGRect(x: x + 5, y: y + 0, width: 100, height: 100)
         unoButton.layer.cornerRadius = 20
@@ -23,63 +24,23 @@ class ViewController: UIViewController, UITableViewDelegate{
         unoButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         return unoButton
     }
-
-//    class AllButton {
-//
-//
-//       public func createButton() {
-//
-//
-//            let unoButton = UIButton (type: .system) as UIButton
-//            unoButton.backgroundColor = .red
-//            unoButton.setTitle("1", for: .normal)
-//            unoButton.frame = CGRect(x: 5, y: 200, width: 100, height: 100)
-//            unoButton.layer.cornerRadius = 20
-//
-//
-//            unoButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-//
-//
-//
-//        }
-//
-//    }
     
+    func textButton(_ number2: String, x: Int, y: Int) -> UIButton {
+        let unoButton = UIButton (type: .system) as UIButton
+        unoButton.setTitleColor(.white, for: .normal)
+        unoButton.backgroundColor = .gray
+        unoButton.setTitle("\(number2)", for: .normal)
+        unoButton.frame = CGRect(x: x + 5, y: y + 0, width: 100, height: 100)
+        unoButton.layer.cornerRadius = 20
+        
+        unoButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return unoButton
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let button_1 = UIButton (type: .system) as UIButton
-//        button_1.backgroundColor = .blue
-//        button_1.setTitle("1", for: .normal)
-//        button_1.frame = CGRect(x: 5, y: 400, width: 100, height: 100)
-//        button_1.layer.cornerRadius = 20
-//
-//
-//        button_1.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-//
-//        self.view.addSubview(button_1)
-//
-//        let button_2 = UIButton (type: .system) as UIButton
-//        button_2.backgroundColor = .blue
-//        button_2.setTitle("2", for: .normal)
-//        button_2.frame = CGRect(x: 106, y: 400, width: 100, height: 100)
-//        button_2.layer.cornerRadius = 20
-//
-//        button_2.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-//
-//        self.view.addSubview(button_2)
-//
-//        let button_3 = UIButton (type: .system) as UIButton
-//        button_3.backgroundColor = .blue
-//        button_3.setTitle("3", for: .normal)
-//        button_3.frame = CGRect(x: 207, y: 400, width: 100, height: 100)
-//        button_3.layer.cornerRadius = 20
-//
-//        button_3.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-//
-//        self.view.addSubview(button_3)
-//
         let clearButton = UIButton (type: .system) as UIButton
         clearButton.backgroundColor = .blue
         clearButton.setTitle("Clear", for: .normal)
@@ -113,6 +74,7 @@ class ViewController: UIViewController, UITableViewDelegate{
             
             self.view.addSubview(createButton(number1,x: x ,y: y))
             
+            
             if (number1 == 3 ) || (number1 == 6) {
                 x = 0
                 y = y + 105
@@ -120,22 +82,20 @@ class ViewController: UIViewController, UITableViewDelegate{
             
             x = x + 105
                 }
-//            if (number1 == 6 ) {
-//                x = 0
-//                y = y + 105
-//            }
-//            
-        }
 
+        }
+        
+        self.view.addSubview(textButton("+",x: 0 ,y: 450))
+        self.view.addSubview(textButton("-",x: 207 ,y: 450))
     }
     
-    @objc private func buttonTapped(_ sender: UIButton) {
+    @objc private func buttonTapped(sender: UIButton) {
         MyTextField.text = sender.titleLabel?.text
        
     }
         
    @objc private func ClearButtonTapped() {
-        MyTextField.text = "Clear"
+        MyTextField.text = "0"
        
        
    }
